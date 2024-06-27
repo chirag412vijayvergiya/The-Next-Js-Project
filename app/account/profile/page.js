93b76b33ba29126965e240d0c59a54dea1389f72
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-06-27 22:21:15 +0530

    Added Cabin page functionality

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,5 +1,10 @@
 import SelectCountry from "@/app/_components/SelectCountry";
 
+export const metadata = {
+  title: "Update Profile",
+  description: "Update your guest profile",
+};
+
 export default function Page() {
   // CHANGE
   const countryFlag = "pt.jpg";
