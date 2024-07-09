6a175d27a78e6d8e88d0e77e316eafb1ac332277
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-07-09 17:39:42 +0530

    Added Filter

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,17 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
+
+// export const revalidate = 0;
 
 export const metadata = {
   title: "Cabins",
   description: "Cabins Page",
 };
 
-export default function Page() {
-  // CHANGE
-
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +25,12 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
