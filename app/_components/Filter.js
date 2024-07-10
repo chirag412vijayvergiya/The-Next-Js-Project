e89dd2e394db6085625d0278410e9ec24c677010
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-07-10 22:39:31 +0530

    Added Calender and Reservation in Cabinid page

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -51,7 +51,7 @@ function Filter() {
 function Button({ filter, handleFilter, activeFilter, children }) {
   return (
     <button
-      className={`px-5 py-2 hover:bg-primary-700 ${
+      className={`px-4 py-2 hover:bg-primary-700 mx-1 ${
         filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
       }`}
       onClick={() => handleFilter(filter)}
