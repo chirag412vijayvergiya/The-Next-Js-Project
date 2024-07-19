fcefc132dcf3c7b4dbf266e56ffe5ca2045eddc2
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-07-19 08:55:37 +0530

    Use COntext Api

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,9 +1,9 @@
 "use client";
 
 import { isWithinInterval } from "date-fns";
-import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -16,7 +16,7 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, cabin, bookedDates }) {
-  const [range, setRange] = useState({ from: undefined, to: undefined });
+  const { range, setRange, resetRange } = useReservation();
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
@@ -74,7 +74,7 @@ function DateSelector({ settings, cabin, bookedDates }) {
         {range.from || range.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>
