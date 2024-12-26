3fd92f4a4a82328a17bf16efb0b518b477c379d2
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-12-26 17:39:28 +0530

    Delete Bookings

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -186,6 +186,7 @@ export async function createBooking(newBooking) {
 // UPDATE
 
 // The updatedFields is an object which should ONLY contain the updated data
+/*
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
     .from("guests")
@@ -228,3 +229,4 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+*/
--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,18 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
   description: "Account Page",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  console.log(session);
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
-    <div>
-      <h1>Account Page</h1>
-    </div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
   );
 }
